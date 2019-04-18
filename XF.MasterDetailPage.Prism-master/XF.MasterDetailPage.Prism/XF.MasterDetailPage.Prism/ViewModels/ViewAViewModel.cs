3526974d149a08bd9ec0c13264e1e8451648896f
begreal <48756184+begreal@users.noreply.github.com>
2019-04-18 14:55:29 +0200

    Add files via upload

---
+++ XF.MasterDetailPage.Prism-master/XF.MasterDetailPage.Prism/XF.MasterDetailPage.Prism/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace XF.MasterDetailPage.Prism.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private DelegateCommand _navigateCommand;
+        private readonly INavigationService navigationService;
+
+        public DelegateCommand NavigateCommand =>
+            _navigateCommand ?? (_navigateCommand = new DelegateCommand(ExecuteNavigateCommand));
+
+        public ViewAViewModel(INavigationService navigationService)
+        {
+            Title = "ViewA";
+            this.navigationService = navigationService;
+        }
+
+        ~ViewAViewModel()
+        {
+            Console.WriteLine(this + " DESTRUCTOR");
+        }
+
+        async void ExecuteNavigateCommand()
+        {
+            await navigationService.NavigateAsync("ViewB");
+        }
+    }
+}
